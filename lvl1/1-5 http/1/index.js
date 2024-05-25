c07a4d69b957d87155527b8cc239be73b7fa9270
Abramov Iurii <abtank@bk.ru>
2024-05-26 00:49:43 +0300

    1.5. Http module. Переменные окружения

---
+++ lvl1/1-5 http/1/index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+const myAPIKey = process.env.myAPIKey
+const url = `http://data.fixer.io/api/latest?access_key=${myAPIKey}&symbols=USD,EUR,RUB`
+
+http.get(url, (res) => {
+    const { statusCode } = res
+    if (statusCode !== 200) {
+        console.log(`statusCode: ${statusCode}`);
+        return
+    }
+
+    res.setEncoding('utf8')
+    let rowData = ''
+    res.on('data', (chank) => { rowData += chank })
+    res.on('end', () => {
+        let parseData = JSON.parse(rowData)
+        console.log(parseData)
+    })
+
+}).on('error', (err) => {
+    console.log(err)
+})
