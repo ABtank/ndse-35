4968762435309a9f13784ce5751f032c23bd3002
Abramov Iurii <abtank@bk.ru>
2024-09-02 03:47:57 +0300

    dz-4-006 Book BooksService BooksController BookModule

--- lvl1/4-NestJs/dz/bookstore/src/app.module.ts
+++ lvl1/4-NestJs/dz/bookstore/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { BooksController } from './books/books.controller';
+import { logger } from './logger.middleware.function';
 
 @Module({
-  imports: [],
+  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // настройка посредника
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(logger).forRoutes(BooksController);
+  }
+}
