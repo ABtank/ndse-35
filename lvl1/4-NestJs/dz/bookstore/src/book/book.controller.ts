6653afd66ec3467a1c5749bf88d5c122a4f76b63
Abramov Iurii <abtank@bk.ru>
2024-09-28 04:24:07 +0300

    dz-4-010 validations Interceptors, pipes

--- lvl1/4-NestJs/dz/bookstore/src/book/book.controller.ts
+++ lvl1/4-NestJs/dz/bookstore/src/book/book.controller.ts
@@ -6,18 +6,25 @@ import {
   Param,
   Post,
   Put,
+  UseInterceptors,
+  UsePipes,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDocument } from './schemas/book.schema';
 import { IparamId } from './interfaces/param-id';
 import { UpdateBookDto } from './interfaces/dto/update-book';
 import { CreateBookDto } from './interfaces/dto/create-book';
+import { ResponseInterceptor } from 'src/response.interceptor';
+import { ValidateCreateBookDtoPipe } from './validate.createbookdto.pipe';
+import { JoiValidationUpdateBookPipe } from './interfaces/validation/joi.validation.updatebook.pipe';
 
+@UseInterceptors(ResponseInterceptor)
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Post()
+  @UsePipes(ValidateCreateBookDtoPipe)
   public create(@Body() body: CreateBookDto): Promise<BookDocument> {
     return Promise.resolve(this.bookService.create(body));
   }
@@ -30,7 +37,7 @@ export class BookController {
   @Put(':id')
   update(
     @Param() { id }: IparamId,
-    @Body() body: UpdateBookDto,
+    @Body(JoiValidationUpdateBookPipe) body: UpdateBookDto,
   ): Promise<BookDocument[]> {
     return this.bookService.update(id, body);
   }
